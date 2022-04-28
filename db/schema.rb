d8713ac157953e7f6d68da406dc91cd7545607cd
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-04-28 18:31:30 +0630

    Added portfolio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_063225) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_115315) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
