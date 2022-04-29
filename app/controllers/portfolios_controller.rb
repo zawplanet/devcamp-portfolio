9a8e05d3923f55953ce7709281eae7e675cec260
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-04-29 19:28:13 +0630

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,9 +2,11 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
+
 	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -16,9 +18,11 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
   def edit
   	@portfolio_item = Portfolio.find(params[:id])
   end
+
   def update
   	@portfolio_item = Portfolio.find(params[:id])
   	
@@ -30,4 +34,9 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ puts "10 Blog created."
 		title: "My portfolio title: #{portfolio_item}",
 		subtitle: "My great Service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		main_image: "https://images.pexels.com/photos/11395820/pexels-photo-11395820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+		main_image: "https://designmodo.com/wp-content/uploads/2014/07/free-photos.jpg",
 		thumb_image: "https://lehighvalleyicearena.com/wp-content/uploads/2014/08/300x200.gif"
 		)
 end
