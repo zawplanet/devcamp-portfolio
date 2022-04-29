767df71adf1bf236ebff51a3e17d2daf87e97855
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-04-29 12:27:11 +0630

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ puts "10 Blog created."
 		subtitle: "My great Service",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		main_image: "https://images.pexels.com/photos/11395820/pexels-photo-11395820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-		thumb_image: "https://placehold.it/350x200"
+		thumb_image: "https://lehighvalleyicearena.com/wp-content/uploads/2014/08/300x200.gif"
 		)
 end
 
