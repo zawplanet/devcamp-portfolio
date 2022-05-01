9e9ad7b8c3dc00dffd8124bb295c7f416554761c
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-05-01 19:30:46 +0630

    Implemented relational database to Topic and Blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_30_053646) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_01_123358) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,7 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_053646) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.integer "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -49,4 +51,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_30_053646) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
