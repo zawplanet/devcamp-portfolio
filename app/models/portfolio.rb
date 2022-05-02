4e5adf65cb116693a8855272a6ce06ea47bc1f05
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-05-02 18:33:24 +0630

    Integrated nested attributes for portfolio model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,7 @@
 class Portfolio < ApplicationRecord
 	has_many :technoloogies
+	accepts_nested_attributes_for :technoloogies,
+								   reject_if: lambda { |attrs| attrs['name'].black? }
 
 	include Placeholder
 
