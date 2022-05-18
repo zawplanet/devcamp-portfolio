904d08f583a333b36491be6d10e5a6b6535e3f98
Zaw Lay Pyay Thar <zawlaypyaythar2@gmail.com>
2022-05-18 19:46:55 +0630

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
         (link_to "Login", new_user_session_path)
       end
 	end
+
+	def source_helper(layout_name)
+	  if session[:source]
+	  	greeting = "Thanks for visiting me from #{session[:source]} and you are on #{layout_name} layout"
+        content_tag(:h3, greeting, style: "color: red;", class: "source-greeting")
+      end
+	end
 end
